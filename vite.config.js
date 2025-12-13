9f373affe1038c65520a4250cf075147e5d1c95a
amenoni <andrewj.menoni@gmail.com>
2025-12-13 17:17:25 -0500

    change manifest paths

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/token-note/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        popover: resolve(__dirname, "popover.html"),
-        background: resolve(__dirname, "background.html"),
-      },
-    },
-  },
 });
