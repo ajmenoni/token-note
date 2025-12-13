dd49437252342023796987496c9c74a91ccb5b1e
amenoni <andrewj.menoni@gmail.com>
2025-12-13 15:08:31 -0500

    add docs for distribution

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/token-note/",
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
     },
-    allowedHosts: ["zshops-planet-emacs-savannah.trycloudflare.com"],
+    allowedHosts: ["dam-installing-hours-spin.trycloudflare.com"],
   },
 });
