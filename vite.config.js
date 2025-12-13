d7ed0dc6880cc29fcf3cb8d3a8f9e00cb530c901
amenoni <andrewj.menoni@gmail.com>
2025-12-13 18:05:32 -0500

    update deploy from

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/token-note/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"), // Your main React entry
+        background: resolve(__dirname, "background.html"), // Copy background.html as a separate entry
+      },
+    },
+  },
 });
